24ca447c7c422be9033f80c2690a1dfabfed828a
suenefuzzo <suenequeiroz@hotmail.com>
2023-03-02 16:03:31 -0300

    fix: import font

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap');
+/* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap'); */
     
     /* http://meyerweb.com/eric/tools/css/reset/ 
    v2.0 | 20110126
